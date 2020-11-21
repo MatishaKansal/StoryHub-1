fd32172720269dd908c766a7048c02b0ecdf7844
MatishaKansal <64193940+MatishaKansal@users.noreply.github.com>
2020-11-21 22:20:30 +0530

    Add files via upload

---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, TouchableOpacity, Text, TextInput } from "react-native";
+
+export default class ReadStoryScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text style={{ margin: 150 }}>Read Story</Text>
+      </View>
+    );
+  }
+}
